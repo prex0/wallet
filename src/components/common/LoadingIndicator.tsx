30ff743496f16a85bc076662af7cdc70812c2053
syuhei176 <syuhei176@gmail.com>
2024-09-27 08:34:01 +0900

    fix build

--- src/components/common/LoadingIndicator.tsx
+++
@@ -1,9 +0,0 @@
-export const LoadingIndicator = () => {
-  return (
-    <div className="flex justify-center">
-      <div className="animate-ping h-2 w-2 bg-white rounded-full"></div>
-      <div className="animate-ping h-2 w-2 bg-white rounded-full mx-4"></div>
-      <div className="animate-ping h-2 w-2 bg-white rounded-full"></div>
-    </div>
-  )
-}
