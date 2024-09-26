11fe910a98331e2e0d52a8190e659a6ce2b56243
syuhei176 <syuhei176@gmail.com>
2024-09-26 16:55:09 +0900

    add title in WalletCreation

--- src/Layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Outlet } from "react-router-dom";
-import { EmbeddedWallet } from '@prex0/uikit/wallet';
-
-export function Layout() {
-  return (<EmbeddedWallet title="Prex Wallet"><Outlet /></EmbeddedWallet>)
-}
--- src/pages/WalletLayout.tsx
+++ src/pages/WalletLayout.tsx
@@ -7,10 +7,14 @@ import { BottomNav } from '../components/BottomNav';
 import { Header } from '../components/Header';
 import { SIDE_MENU, Sidebar } from '../components/Sidebar';
 
+// WalletLayout component: Manages the layout of the wallet interface
 export function WalletLayout() {
+  // State for controlling the sliding form
   const [isFormOpen, setIsFormOpen] = useState(false);
+  // State for managing the selected menu item
   const [menu, setMenu] = useState<SIDE_MENU>('home');
 
+  // Handler for menu selection
   const handleSelectMenu = useCallback((menu: SIDE_MENU) => {
     if (menu === 'home') {
       setIsFormOpen(false)
@@ -20,20 +24,23 @@ export function WalletLayout() {
     }
   }, [isFormOpen, setIsFormOpen, setMenu])
 
-  return <EmbeddedWallet>
+  return <EmbeddedWallet title="Prex Wallet">
     <div className="flex">
       <Sidebar onSelectMenu={handleSelectMenu}/>
       <div className='w-full'>
+        {/* Header component */}
         <Header />
+        {/* HomePage component, hidden on mobile */}
         <div className='md:block hidden w-full'>
           <HomePage />
         </div>
       </div>
     </div>
+    {/* Router outlet for nested routes */}
     <Outlet />
+    {/* Sliding form component */}
     <SlidingForm isOpen={isFormOpen} menu={menu} onClose={() => {setIsFormOpen(false)}}/>
+    {/* Bottom navigation component for mobile */}
     <BottomNav />
   </EmbeddedWallet>
 }
-
-
