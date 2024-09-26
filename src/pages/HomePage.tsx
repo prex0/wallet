e046900f3a7868adf86f846cb75b886b3240987c
syuhei176 <syuhei176@gmail.com>
2024-09-26 23:04:32 +0900

    update history view

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
 import {UILabel1} from '@prex0/uikit'
 import { TokenBalance } from '@prex0/uikit/identity';
-import { History, TransferHistory, TransferHistoryItemCustom, SwapHistory, SwapHistoryItemCustom } from '@prex0/uikit/history';
+import { History, TransferHistory, TransferHistoryItemCustom, LinkTransferHistory, LinkTransferHistoryItemCustom,SwapHistory, SwapHistoryItemCustom } from '@prex0/uikit/history';
 import { USDC_TOKEN } from '../constants';
 import { SwapHistoryItemContent } from '../components/SwapHistoryItem';
+import { useState } from 'react';
+import { cn, color } from '@prex0/uikit/styles';
+
+enum HistoryTab {
+  TRANSFER = 'transfer',
+  LINK_TRANSFER = 'linkTransfer',
+  SWAP = 'swap',
+}
 
 export const HomePage = () => {
+  const [activeTab, setActiveTab] = useState(HistoryTab.TRANSFER);
+
   return <div className='p-4 space-y-4'>
     <div>
       <UILabel1>Assets</UILabel1>
@@ -13,17 +23,60 @@ export const HomePage = () => {
       </div>
     </div>
     <div>
-      <UILabel1>Transfer History</UILabel1>
-      <History token={USDC_TOKEN.address as `0x${string}`}>
-        <TransferHistory>
-          <TransferHistoryItemCustom/>
-        </TransferHistory>
-        <SwapHistory>
-          <SwapHistoryItemCustom>
-            <SwapHistoryItemContent />
-          </ SwapHistoryItemCustom>
-        </SwapHistory>
-      </History>
+      <UILabel1>History</UILabel1>
+      <div className="flex space-x-2 mb-2">
+        <button
+          className={cn(
+            'px-0 py-1 rounded',
+            activeTab === HistoryTab.TRANSFER ? color.primary : color.disabled
+          )}
+          onClick={() => setActiveTab(HistoryTab.TRANSFER)}
+        >
+          Transfer
+        </button>
+        <button
+          className={cn(
+            'px-2 py-1 rounded',
+            activeTab === HistoryTab.LINK_TRANSFER ? color.primary : color.disabled
+          )}
+          onClick={() => setActiveTab(HistoryTab.LINK_TRANSFER)}
+        >
+          Link Transfer
+        </button>
+        <button
+          className={cn(
+            'px-2 py-1 rounded',
+            activeTab === HistoryTab.SWAP ? color.primary : color.disabled
+          )}
+          onClick={() => setActiveTab(HistoryTab.SWAP)}
+        >
+          Swap
+        </button>
+      </div>
+        {activeTab === HistoryTab.TRANSFER && (
+          <History transferHistoryEnabled={true} linkTransferHistoryEnabled={false} swapHistoryEnabled={false}>
+            <TransferHistory>
+              <TransferHistoryItemCustom/>
+            </TransferHistory>
+          </History>
+        )}
+        {activeTab === HistoryTab.LINK_TRANSFER && (
+          <History transferHistoryEnabled={false} linkTransferHistoryEnabled={true} swapHistoryEnabled={false}>
+
+          <LinkTransferHistory>
+            <LinkTransferHistoryItemCustom/>
+          </LinkTransferHistory>
+          </History>
+        )}
+        {activeTab === HistoryTab.SWAP && (
+          <History transferHistoryEnabled={false} linkTransferHistoryEnabled={false} swapHistoryEnabled={true}>
+          <SwapHistory>
+            <SwapHistoryItemCustom>
+              <SwapHistoryItemContent />
+            </SwapHistoryItemCustom>
+          </SwapHistory>
+          </History>
+        )}
     </div>
   </div>
 }
