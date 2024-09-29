781ee6004a2af3e07df03dc53409fa469af45d77
syuhei176 <syuhei176@gmail.com>
2024-09-29 17:59:29 +0900

    Made the bottom navigation easier to select

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -4,24 +4,21 @@ import { Link } from "react-router-dom";
 export const BottomNav = () => {
   return (
     <UIPanel className="fixed bottom-0 left-0 w-full md:hidden">
-      <nav className="flex justify-around py-4">
+      <nav className="flex justify-around py-0">
         <div className="flex flex-col items-center">
-          <div className="text-xl" />
-          <UILabel1>
-            <Link to="/">Home</Link>
-          </UILabel1>
+            <Link to="/">
+              <UILabel1 className="p-4">Home</UILabel1>
+            </Link>
         </div>
         <div className="flex flex-col items-center">
-          <div className="text-xl" />
-          <UILabel1>
-            <Link to="send">Send</Link>
-          </UILabel1>
+          <Link to="send">
+            <UILabel1 className="p-4">Send</UILabel1>
+          </Link>
         </div>
         <div className="flex flex-col items-center">
-          <div className="text-xl" />
-          <UILabel1>
-            <Link to="swap">Swap</Link>
-          </UILabel1>
+          <Link to="swap">
+            <UILabel1 className="p-4">Swap</UILabel1>
+          </Link>
         </div>
       </nav>
     </UIPanel>
