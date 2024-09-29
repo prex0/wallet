548774b5239028eeb9d8ccc95e9df9a4c370af10
syuhei176 <syuhei176@gmail.com>
2024-09-29 09:30:48 +0900

    split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
@@ -28,5 +29,15 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [react(), visualizer()],
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "@prex0/uikit"]
+        },
+      },
+    },
+  },
 })
