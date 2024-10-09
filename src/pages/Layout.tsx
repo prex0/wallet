baf533f4576fe367ed290e581c03aed0db375c68
syuhei176 <syuhei176@gmail.com>
2024-10-09 11:54:51 +0900

    passkey not support view

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,18 @@
-import { EmbeddedWallet } from "@prex0/uikit/wallet";
+import { EmbeddedWallet, DevicePasskeySupport } from "@prex0/uikit/wallet";
 import { Outlet } from "react-router-dom";
 
 export function Layout() {
-  return <EmbeddedWallet title="Prex Wallet">
+  return <DevicePasskeySupport notSupportedComponent={<NotSupported/>}><EmbeddedWallet title="Prex Wallet">
     <Outlet />
-  </EmbeddedWallet>
+  </EmbeddedWallet></DevicePasskeySupport>
+}
+
+
+function NotSupported() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4 text-center">Device not supported</h1>
+      <p className="text-center">Please use a device that supports passkeys</p>
+    </div>
+  );
 }
