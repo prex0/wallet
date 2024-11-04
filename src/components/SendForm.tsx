0571579f5d06375146739cca793e30a8cc213619
syuhei176 <syuhei176@gmail.com>
2024-11-04 14:34:52 +0900

    only usdc can be sent

--- src/components/SendForm.tsx
+++ src/components/SendForm.tsx
@@ -5,7 +5,7 @@ import { USDC_TOKEN_ARBITRUM } from '@prex0/uikit';
 export const SendForm = () => {
   return (
     <div className="px-3">
-      <RichTransferCard token={USDC_TOKEN_ARBITRUM} getURL={getURL} onSuccess={() => console.log('success')} />
+      <RichTransferCard token={USDC_TOKEN_ARBITRUM} disableTokenSelection amount='' getURL={getURL} onSuccess={() => console.log('success')} />
     </div>
   );
 };
