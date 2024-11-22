f66a11f84c9ce5b395bd8d1d24b0e22e7bff7179
syuhei176 <syuhei176@gmail.com>
2024-11-23 05:09:52 +0900

    three seconds

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster"
 import { openChrome } from "@prex0/uikit/wallet";
 import { Button } from "../components/ui/button";
+import { useEffect, useState } from "react";
 
 export function Layout() {
   return <DevicePasskeySupport notSupportedComponent={<NotSupported/>}><EmbeddedWallet title="Prex Wallet">
@@ -13,10 +14,28 @@ export function Layout() {
 
 
 function NotSupported() {
+  const [seconds, setSeconds] = useState(3);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+
+    const timeout = setTimeout(() => {
+      openChrome();
+    }, 3000);
+
+    return () => {
+      clearInterval(timer);
+      clearTimeout(timeout);
+    };
+  }, [setSeconds]);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold mb-4 text-center">Device not supported</h1>
       <p className="text-center">Please use a device that supports passkeys</p>
+      <p className="text-center">Opening Chrome in {seconds} seconds...</p>
       <Button onClick={openChrome}>Open Chrome?</Button>
     </div>
   );
