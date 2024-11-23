8069091e9c013e5e2a6627e9158dbb1f82941767
syuhei176 <syuhei176@gmail.com>
2024-11-23 09:17:34 +0900

    update

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster"
 import { openChrome } from "@prex0/uikit/wallet";
 import { Button } from "../components/ui/button";
-import { useEffect, useState } from "react";
 
 export function Layout() {
   return <DevicePasskeySupport notSupportedComponent={<NotSupported/>}><EmbeddedWallet title="Prex Wallet">
@@ -12,33 +11,12 @@ export function Layout() {
   </EmbeddedWallet></DevicePasskeySupport>
 }
 
-
 function NotSupported() {
-  const [seconds, setSeconds] = useState(4);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (seconds > 0) {
-        setSeconds((prev) => prev - 1);
-      }
-    }, 1000);
-
-    const timeout = setTimeout(() => {
-      openChrome();
-    }, 4000);
-
-    return () => {
-      clearInterval(timer);
-      clearTimeout(timeout);
-    };
-  }, [setSeconds]);
-
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold mb-4 text-center">Device not supported</h1>
       <p className="text-center">Please use a device that supports passkeys</p>
-      {seconds > 0 && <p className="text-center">Opening Chrome in {seconds} seconds...</p>}
-      <Button onClick={openChrome}>Open Chrome?</Button>
+      <Button onClick={openChrome}>Open Chrome</Button>
     </div>
   );
 }
